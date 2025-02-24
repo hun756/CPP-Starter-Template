3e71e7b0d20b35ad79f9a9fae5e681c10eda469e
Mehmet Şükrü Ekemen <ekemenms@hotmail.com>
2025-02-24 03:03:33 +0300

    Refactor GoogleTest integration into a separate CMake module

--- cmake/FindGTest.cmake
+++ cmake/FindGTest.cmake
@@ -1,7 +1,13 @@
 include(FetchContent)
+
 FetchContent_Declare(
     googletest
     GIT_REPOSITORY https://github.com/google/googletest.git
     GIT_TAG release-1.12.1
 )
+
+set(FETCHCONTENT_QUIET OFF)
+
+set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
 FetchContent_MakeAvailable(googletest)
